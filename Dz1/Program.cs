b96f2a4d62de43b9493117fe6faca612d950c778
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-17 12:43:38 +0300

    Dz1

---
+++ Dz1/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите четвертое число ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите пятое число ");
+int y = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+if (a > 0)
+{
+    i++;
+}
+
+if (b > 0)
+{
+    i++;
+}
+
+if (c > 0)
+{
+    i++;
+}
+
+if (z > 0)
+{
+    i++;
+}
+
+if (y > 0)
+{
+    i++;
+}
+
+Console.WriteLine(i);
