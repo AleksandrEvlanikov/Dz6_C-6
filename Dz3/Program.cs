e1b06701ad7821e7135765cad57ee48cfe6b1e15
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-17 13:19:31 +0300

    Dz3

---
+++ Dz3/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] number = new int[5];
+Random rnd = new Random();
+
+int sum = 0;
+int sum1 = 0;
+
+for( int i = 0; i < 5; i++)
+{
+    number[i] = rnd.Next(1, 60);
+}
+
+
+sum = number[0] + number[4];
+sum1 = number[1] + number[3];
+
+sum = sum + sum1;
+
+Console.WriteLine(sum);
